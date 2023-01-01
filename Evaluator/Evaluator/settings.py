99a844095a1fd5bc5dfc999dd12f77aa2f7f544a
Claudio <claudio.rtega2701@gmail.com>
2023-01-01 15:36:26 +0100

    set up whitenoise

--- Evaluator/Evaluator/settings.py
+++ Evaluator/Evaluator/settings.py
@@ -2,7 +2,7 @@
 Django settings for Evaluator project.
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from pathlib import Path
 import os
 
 if not os.getenv("IN_PRODUCTION"):
@@ -10,7 +10,8 @@ if not os.getenv("IN_PRODUCTION"):
     from dotenv import load_dotenv
     load_dotenv()
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
@@ -18,11 +19,12 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS")]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(",")
 
 # Application definition
 
 INSTALLED_APPS = (
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -32,11 +34,13 @@ INSTALLED_APPS = (
     'django.contrib.gis',
     'django.contrib.admindocs',
 
-    # Add your apps here
-    'django_extensions',
+    # Third party apps
+    'django_extensions', 
+    'rest_framework',
     # 'django_nose',
     # 'debug_toolbar',
-    'rest_framework',
+
+    # Local apps
     'tables',
     'PSM',
     'upload',
@@ -81,7 +85,7 @@ ROOT_URLCONF = 'Evaluator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -159,18 +163,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
 # WhiteNoise Configuration
 # http://whitenoise.evans.io/en/stable/#quickstart-for-django-apps
+STATIC_URL = '/static/'
+STATIC_ROOT = "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
-# Static files (CSS, JavaScript, Images)
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = '/static/'
-
 # TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-
 # NOSE_ARGS = ['--nocapture']
 
 LOGGING = {
@@ -216,7 +216,6 @@ LOGGING = {
 
 SESSION_COOKIE_AGE = 86400
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-
 
 # tables.ResultsChart: (models.W042) Auto-created primary key used when not defining a primary key type, by default 'django.db.models.AutoField'.
 #         HINT: Configure the DEFAULT_AUTO_FIELD setting or the AppConfig.default_auto_field attribute to point to a subclass of AutoField, e.g. 'django.db.models.BigAutoField'.
